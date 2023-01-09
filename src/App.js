9d7a092fc3a739fbbdd7876df1e1f0361e049d7a
MazenShebeta <mazenshebeta@gmail.com>
2023-01-09 23:39:22 +0200

    added Header and Home components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TopBar from './topbar/TopBar';
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <>
       <TopBar />
+      <Home />
     </>
   );
 }

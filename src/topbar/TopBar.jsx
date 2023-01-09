0693ae231dab42e62326ddc96efa4b40d2b882ce
MazenShebeta <mazenshebeta@gmail.com>
2023-01-09 17:35:08 +0200

    created TopBar

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./TopBar.css";
+
+export default function TopBar() {
+  return (
+    <div className="topbar">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img className="topImg" src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
